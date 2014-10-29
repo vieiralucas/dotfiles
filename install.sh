0ac2fff41d8a402428140d16c73615c1b36d1880
Lucas Vieira <vieiralucas4@gmail.com>
2014-10-29 00:58:09 -0200

    add script to automatically install dotfiles

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]; then
+    echo "Please inform the OS"
+    echo "Options are: 'ubuntu'"
+    echo "Example: 'install ubuntu'"
+    exit 1
+fi
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+if [[ $1 -eq "ubuntu" ]]; then
+    bash $DIR/ubuntu/install.sh
+    exit 0;
+fi
