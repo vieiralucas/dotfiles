938ce7694f5c95a6e30e29325f245afa3263075e
Lucas Vieira <vieiralucas4@gmail.com>
2016-03-18 15:52:08 -0300

    Simplify tmux install

--- ubuntu/tmux/install.sh
+++ ubuntu/tmux/install.sh
@@ -6,8 +6,5 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 sudo apt-get install -y tmux > /dev/null
 
 # config tmux
-cp $DIR/.tmux.conf ~/.tmux.conf
-cp $DIR/.tmux-theme.sh ~/.tmux-theme.sh
-cp $DIR/.battery-indicator.sh ~/.battery-indicator.sh
-cp $DIR/.dev-tmux-session ~/.dev-tmux-session
+cp $DIR/.* ~/
 
