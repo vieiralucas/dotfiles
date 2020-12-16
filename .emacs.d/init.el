04d7516750aef2e16bd39ba88ff4e9b97f8afa9d
Lucas Vieira <vieiralucas4@gmail.com>
2020-12-15 21:13:27 -0300

    emacs?

---
+++ .emacs.d/init.el
@@ -0,0 +1,71 @@
+;; Set up package.el to work with MELPA
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(defvar my-packages
+  '(
+    use-package
+    evil
+    tide
+    company))
+
+;; (package-refresh-contents)
+
+(dolist (p my-packages)
+  (unless (package-installed-p p))
+    (package-install p))
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
+
+;; enable line numbers
+(global-display-line-numbers-mode)
+
+;; set relative line numbers
+(setq display-line-numbers-type 'relative)
+
+;; disable syntax highlighting
+(global-font-lock-mode 0)
+
+;; disable menu-bar
+(menu-bar-mode -1)
+
+;; start tide setup
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+;; finish tide setup
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(company tide use-package fzf evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
